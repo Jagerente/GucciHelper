e51cccc6463d08aa0e5ad991da54005ceee1ea6c
Jager Ente <JustlPains@gmail.com>
2021-02-17 10:26:06 +0300

    Added method to take custom emoji object by name

---
+++ DiscordBot/Extensions/GuildExtensions.cs
@@ -0,0 +1,17 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Extensions
+{
+    public static class GuildExtensions
+    {
+        public static async Task<DiscordEmoji> GetByName(this DiscordGuild guild, string name)
+        {
+            IReadOnlyList<DiscordGuildEmoji> emojis = await guild.GetEmojisAsync();
+            return emojis.FirstOrDefault(emoji => emoji.Name == name);
+        }
+    }
+}
